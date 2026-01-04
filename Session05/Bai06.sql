91a5e55aa3591266a1160f04ded9b2bc428dd3e9
phuongngo17 <139088810+phuongngo17@users.noreply.github.com>
2026-01-04 21:25:31 +0700

    Change order of product price sorting to ascending

--- Session05/Bai06.sql
+++ Session05/Bai06.sql
@@ -1,8 +1,9 @@
 use bai_01;
 select status, price from products 
 where price between 1000000 and 3000000 and status ='active'
-order by price desc
+order by price asc
 limit 10 offset 0;
 select status, price from products 
-order by price asc 
+where price between 1000000 and 3000000 and status ='active'
+order by price asc
 limit 10 offset 10;
